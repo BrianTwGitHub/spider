fc4d85d02ba66cf161640e2cbb66f782c039d8f6
brian <brian.huang@sis.ai>
2021-01-14 15:34:49 +0800

    fix 跨年度更新日期 issue

--- src/main/java/com/youlin/spider/demo/utils/DateUtils.java
+++ src/main/java/com/youlin/spider/demo/utils/DateUtils.java
@@ -10,6 +10,9 @@ public class DateUtils {
     public static Date parseUpdateDate(String lastUpdateDate) {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd更新 HH:mm:ss");
         LocalDate date = LocalDate.parse(LocalDate.now().getYear() + "/" + lastUpdateDate + " 00:00:00", dateTimeFormatter);
+        if (date.toEpochDay() > LocalDate.now().toEpochDay()) {
+            date = LocalDate.parse((LocalDate.now().getYear() - 1) + "/" + lastUpdateDate + " 00:00:00", dateTimeFormatter);
+        }
         ZonedDateTime zonedDateTime = date.atStartOfDay(ZoneId.systemDefault());
         return Date.from(zonedDateTime.toInstant());
     }
